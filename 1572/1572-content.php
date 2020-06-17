234552dcc495261a3a671ee365d7701a26b0e4b3
huynhnhutlinh99 <40021780+huynhnhutlinh99@users.noreply.github.com>
2020-06-17 09:05:55 +0700

    upload new
    upload new, last version

---
+++ 1572/1572-content.php
@@ -0,0 +1,58 @@
+
+<div class="type-1572">
+    <div class="container">
+        <h1 class="nl-h1">MY FAVOURITE S/S 2020 PIECES ON SALE</h1>
+        <ul class="ul-list-link">
+            <li> 
+                <a href="#"><span class="nl-link-s10"> April 29, 2020 </span></a>
+            </li>
+            <li> 
+                <a href="#"><span class="nl-link-s10">NO COMMENTS</span></a>
+            </li>
+            <li> 
+                <a href="#"><span class="nl-link-s10">Fashion advice</span></a><span class="nl-link-s10">,</span>
+                <a href="#"><span class="nl-link-s10">Shop the look</span></a>
+            </li>
+        </ul>
+        <div class="row nl-row-style-10">
+            <div class="col-md-6 col-sm-12 nl-ul-s10">
+                <p>
+                    <span class="nl-text-firt-big">
+                        E
+                    </span>
+                    <span>gestas augue ac molestie pharetra. Sed porta dui quis imperdiet consectetur. Vivamus tincidunt, turpis non pulvinar scelerisque, nunc arcu blandit diam, ac convallis lectus sem in odio. </span>
+                </p>
+                <p>Nulla tincidunt tincidunt sem, sit amet condimentum ex mollis rutrum. Pellentesque habitant morbi tristique!</p>
+                <p>Nulla tincidunt tincidunt sem, sit amet condimentum ex mollis rutrum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+            </div>
+            <div class="col-md-6 col-sm-12">
+                <ul class="nl-ul-s10">
+                    <li class="nl-li-s10">
+                        <i aria-hidden="true" class="fas fa-check"></i>
+                        <span>Vivamus purus dolor, auctor iaculis leo.</span>
+                    </li>
+                    <li class="nl-li-s10">
+                        <i aria-hidden="true" class="fas fa-check"></i>
+                        <span>Sit amet pretium eget,amet condimentum lorem ipsum dolorex molli tincidunt!</span>
+                    </li>
+                    <li class="nl-li-s10">
+                        <i aria-hidden="true" class="fas fa-check"></i>
+                        <span>Nulla tincidunt tincidunt sem, sit amet condimentum.</span>
+                    </li>
+                    <li class="nl-li-s10">
+                        <i aria-hidden="true" class="fas fa-check"></i>
+                        <span>Vivamus purus dolor, auctor iaculis leo.</span>
+                    </li>
+                    <li class="nl-li-s10">
+                        <i aria-hidden="true" class="fas fa-check"></i>
+                        <span>Leoamet pretium eget, tincidunt in sem.</span>
+                    </li>
+                    <li class="nl-li-s10">
+                        <i aria-hidden="true" class="fas fa-check"></i>
+                        <span>Morbi elit leo - malesuada sit amet pretium eget, tincidunt in sem.</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ 1582/1582-content.php
@@ -0,0 +1,18 @@
+
+<div class="type-1582">
+    <div class="container">
+        <h3>Leave a Reply</h3>
+        <p class="text-center font-12 mar-top-40">Your email address will not be published. Required fields are marked *</p>
+        <form action="#">
+            <textarea name="" id="" class="ip-full" cols="30" rows="10"></textarea>
+            <input type="text" class="ip-full">
+            <input type="text" class="ip-full">
+            <input type="text" class="ip-full">
+            <label for="#" class="font-12">
+                <input type="checkbox">
+                Save my name, email, and website in this browser for the next time I comment.
+            </label> <br>
+            <button type="submit" class="nl-post-cm">POST COMMENT</button>
+        </form>
+    </div>
+</div>
